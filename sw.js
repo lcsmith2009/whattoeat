21153c5f9de610af26f6a3cd4f795ad8ba0dde52
lcsmith2009 <lakel.benfordsmith@aol.com>
2026-05-15 02:31:26 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,15 @@
-const CACHE='whattoeat-v20';const ASSETS=['./','index.html','style.css','script.js','manifest.json','icons/icon-192.png','icons/icon-512.png'];self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
+const CACHE = 'whattoeat-v23';
+const ASSETS = ['./','index.html','style.css?v=23','script.js?v=23','manifest.json?v=23','icons/icon-192.png','icons/icon-512.png'];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(key => key !== CACHE ? caches.delete(key) : null))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE).then(cache => cache.put(event.request, copy)).catch(()=>{});
+    return response;
+  }).catch(() => caches.match(event.request)));
+});
