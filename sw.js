fe98a788223f89c2bd98fd9622d48dff24a72059
lcsmith2009 <lakel.benfordsmith@aol.com>
2026-05-17 09:36:06 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,5 @@
-const CACHE='whattoeat-phase1b-v1';self.addEventListener('install',e=>self.skipWaiting());self.addEventListener('activate',e=>clients.claim());
+const CACHE_NAME='whattoeat-phase-1a-v1';
+const ASSETS=['./','./index.html','./style.css','./script.js','./manifest.json'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))});
